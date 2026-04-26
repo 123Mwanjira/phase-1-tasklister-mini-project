f4df19abf0e5be0c5e20162bf6484851b70b1059
Maurine <maureen.wanjira@gmail.com>
2026-04-26 13:11:46 +0300

    Tested and working task lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.querySelector("#create-task-form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const task = event.target["new-task-description"].value;
+
+  buildToDo(task);
 });
+
+function buildToDo(task) {
+  const taskList = document.querySelector("#tasks");
+
+  const li = document.createElement("li");
+  li.textContent = task;
+
+  taskList.append(li);
+}
